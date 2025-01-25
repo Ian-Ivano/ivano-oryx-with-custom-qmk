0569cff802514bfdb8ddb0abb4ecff19a3f170f8
Ian-Ivano <paulomkali.jnr@gmail.com>
2025-01-25 19:14:49 +0300

    Update keymap.c

--- 6pGVX/keymap.c
+++ 6pGVX/keymap.c
@@ -1,8 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+include "features/achordion.h"
+
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
-
+  
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
   HSV_0_255_255,
@@ -29,6 +31,10 @@ enum tap_dance_codes {
   DANCE_10,
   DANCE_11,
 };
+
+void housekeeping_task_user(void) {
+  achordion_task();
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
@@ -84,6 +90,7 @@ combo_t key_combos[COMBO_COUNT] = {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
